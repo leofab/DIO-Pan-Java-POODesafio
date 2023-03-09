aac63d851cdcfb93aca1d1490485286ef88b177f
leofab <leofabr3@gmail.com>
2023-03-09 11:58:53 -0300

    main test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java Básico");
+        curso1.setDescricao("Descrição curso");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Java Intermediario");
+        curso1.setDescricao("Descrição curso");
+        curso1.setCargaHoraria(12);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria em Java");
+        mentoria1.setDescricao("Descrição Mentoria em Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
     }
 }
